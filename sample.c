70d56fedfc5ee22faf67cf5daeeeb6ee1f5d2819
vise jaffar <visejaffar88@gmail.com>
2022-06-28 22:01:09 +0530

    node

---
+++ sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ints.no=100;
+char sname [10]="fathima";
+float avg =35.65;
+clrscr();
+printf("The student number is :: % d\n",s.no);
+printf("The student name is ::% s\n",s.name);
+printf("The student average is ::%f",avg);
+getch();
+}
